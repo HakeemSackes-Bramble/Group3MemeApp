c3a3e7e17762c2a01313c18ed200fd84c0132936
Akasha Archere <akasha.archer@gmaile.com>
2017-01-22 15:20:59 -0500

    HoneyBun fixes

--- app/src/main/java/nyc/c4q/akashaarcher/group3memestudio/HoneyBunActivity.java
+++ app/src/main/java/nyc/c4q/akashaarcher/group3memestudio/HoneyBunActivity.java
@@ -65,8 +65,6 @@ public class HoneyBunActivity extends Activity {
         }
     }
 
-
-
 //    @Override
 //    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 //        super.onActivityResult(requestCode, resultCode, data);
